18473fcbfa94025d9236c19bfb0eae541b3c29b1
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-26 22:57:06 +0530

    refector

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
-	console.log('MongoDB database connection established successfully');
+	console.log('MongoDB database connection established successfully :');
 });
 
 todoRoutes.route('/').get(function(req, res) {
